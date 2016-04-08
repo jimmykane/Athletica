e18d9b453da0d0ba9d96c6ba19968dcdfbd6ae4b
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-08 15:09:25 +0200

    Add google api helper

---
+++ app/src/main/java/com/dimitrioskanellopoulos/activityface/GoogleAPIHelper.java
@@ -0,0 +1,67 @@
+package com.dimitrioskanellopoulos.activityface;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+
+public class GoogleApiHelper implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = GoogleApiHelper.class.getSimpleName();
+
+    GoogleApiClient mGoogleApiClient;
+
+    public GoogleApiHelper(Context context) {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        connect();
+    }
+
+    public GoogleApiClient getGoogleApiClient() {
+        return this.mGoogleApiClient;
+    }
+
+    public void connect() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    public void disconnect() {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    public boolean isConnected() {
+        if (mGoogleApiClient != null) {
+            return mGoogleApiClient.isConnected();
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+        //mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult.toString());
+    }
+
+}
