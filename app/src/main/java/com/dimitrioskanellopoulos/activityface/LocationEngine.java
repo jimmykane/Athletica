76c76a91aacf7bbbac685d1184360e4f3280c7d3
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-08 15:09:35 +0200

    Use a helper

--- app/src/main/java/com/dimitrioskanellopoulos/activityface/LocationEngine.java
+++ app/src/main/java/com/dimitrioskanellopoulos/activityface/LocationEngine.java
@@ -2,66 +2,34 @@ package com.dimitrioskanellopoulos.activityface;
 
 import android.content.Context;
 import android.location.Location;
-import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 
-public class LocationEngine implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class LocationEngine implements LocationListener {
 
     private static final String TAG = "LocationEngine";
 
-    private GoogleApiClient googleApiClient;
+    private GoogleApiHelper googleApiHelper;
 
     private Location lastKnownLocation;
 
-    public LocationEngine(Context context) {
-        googleApiClient = new GoogleApiClient.Builder(context)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();;
+    public LocationEngine(GoogleApiHelper googleApiHelper) {
+        this.googleApiHelper = googleApiHelper;
     }
 
 
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        Log.e(TAG, "Connected!");
-        // Provides a simple way of getting a device's location and is well suited for
-        // applications that do not require a fine-grained location and that do not need location
-        // updates. Gets the best and most recent location currently available, which may be null
-        // in rare cases when a location is not available.
-        lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        if (lastKnownLocation != null) {
-            Log.e(TAG, "!!!!Location found");
-            return;
+    public Location getLastKnownLocation(){
+        if (googleApiHelper.isConnected()){
+            lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiHelper.getGoogleApiClient());
         }
-    }
-
-    public Location get(){
-        // Try once more to get the loc
-        if (this.googleApiClient != null && this.googleApiClient.isConnected()){
-            this.lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        }
-        return this.lastKnownLocation;
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.e(TAG, "suspended GoogleAPI");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.e(TAG, "connectionFailed GoogleAPI");
+        return lastKnownLocation;
     }
 
     @Override
     public void onLocationChanged(Location location) {
         Log.e(TAG, "Location changed");
     }
-
 }
