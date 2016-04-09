e884bc35beec04a940f2f2b37726c35b6d796dde
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-09 18:51:38 +0200

    Trying to built on pressure

--- app/src/main/java/com/dimitrioskanellopoulos/activityface/PressureSensor.java
+++ app/src/main/java/com/dimitrioskanellopoulos/activityface/PressureSensor.java
@@ -7,12 +7,17 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
+import java.util.Calendar;
+import java.util.HashMap;
+
 public class PressureSensor implements SensorEventListener{
     private static final String TAG = "PressureSensor";
 
     private SensorManager sensorManager;
     private Float lastReading;
     private PressureChangeCallback pressureChangeCallback;
+
+    private HashMap pressureReadings = new HashMap();
 
     public interface PressureChangeCallback {
         void handlePressureValueChanged(Float pressureValue);
@@ -35,6 +40,31 @@ public class PressureSensor implements SensorEventListener{
 
     public Float getLastReading(){
         return lastReading;
+    }
+
+    private void updatePressureReadingsMap(){
+        // Get the 24hour and minute
+        Calendar rightNow = Calendar.getInstance();
+        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+        int minute = rightNow.get(Calendar.MINUTE);
+
+        // Every 5 minutes
+        if ((minute%5) != 0){
+            return;
+        }
+
+        // Construct a key
+        int key = hour+minute;
+        // Clear if it has more than 2 hours
+        if (pressureReadings.size() >= 60){
+            pressureReadings.clear();
+        }
+
+        // If it's not set before
+        if (!pressureReadings.containsKey(key)) {
+            pressureReadings.put(hour + minute, lastReading);
+            Log.d(TAG, "Pressure added to map");
+        }
     }
 
     @Override
