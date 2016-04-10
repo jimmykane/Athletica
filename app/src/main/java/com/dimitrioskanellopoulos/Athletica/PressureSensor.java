db54a598a1ce4f7de740f383be107144f653c555
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-10 22:29:03 +0200

    Add via potential

--- app/src/main/java/com/dimitrioskanellopoulos/Athletica/PressureSensor.java
+++ app/src/main/java/com/dimitrioskanellopoulos/Athletica/PressureSensor.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.hardware.TriggerEventListener;
 import android.util.Log;
 
 import java.util.Calendar;
