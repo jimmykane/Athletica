c4ac08f74027199308ea3be9412e77b5f5361ce2
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-11 22:25:11 +0200

    Tidy up the pressure sensor and add isListening support

--- app/src/main/java/com/dimitrioskanellopoulos/athletica/PressureSensor.java
+++ app/src/main/java/com/dimitrioskanellopoulos/athletica/PressureSensor.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.TriggerEventListener;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.util.Calendar;
@@ -17,73 +18,39 @@ public class PressureSensor implements SensorEventListener{
     private SensorManager sensorManager;
     private final Sensor sensor;
 
-    private Float lastReading;
+    private Boolean isListening;
     private changeCallback changeCallback;
-
-    private HashMap pressureReadings = new HashMap();
 
     public interface changeCallback {
         void handleSensorValueChanged(Float value);
     }
 
-    public PressureSensor(Context context, changeCallback changeCallback) {
+    public PressureSensor(@NonNull Context context, @NonNull changeCallback changeCallback) {
         this.changeCallback = changeCallback;
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
     }
 
     public void startListening(){
-        Log.d(TAG, "Started listening");
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        isListening = true;
+        Log.d(TAG, "Started listening");
     }
 
     public void stopListening(){
-        Log.d(TAG, "Stopped listening");
         sensorManager.unregisterListener(this);
+        isListening = false;
+        Log.d(TAG, "Stopped listening");
     }
 
-    public Float getLastReading(){
-        return lastReading;
-    }
-
-    public Float getAverage(int seconds){
-
-        return 0.0f;
-    }
-
-    private void updatePressureReadingsMap(){
-        // Get the 24hour and minute
-        Calendar rightNow = Calendar.getInstance();
-        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
-        int minute = rightNow.get(Calendar.MINUTE);
-
-        // Every 5 minutes
-        if ((minute%5) != 0){
-            return;
-        }
-
-        // Construct a key
-        int key = hour+minute;
-        // Clear if it has more than 2 hours
-        if (pressureReadings.size() >= 60){
-            pressureReadings.clear();
-        }
-
-        // If it's not set before
-        if (!pressureReadings.containsKey(key)) {
-            pressureReadings.put(hour + minute, lastReading);
-            Log.d(TAG, "Pressure added to map");
-        }
+    public Boolean isListening(){
+        return isListening;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         // Pass the value to the callback
-        if (changeCallback !=null) {
-            changeCallback.handleSensorValueChanged(event.values[0]);
-        }
-        // Update last reading
-        lastReading = event.values[0];
+        changeCallback.handleSensorValueChanged(event.values[0]);
     }
 
     @Override
