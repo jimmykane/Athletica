5278898127718d4f2c5cd4bb9cf1cd9ffdb49ca7
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-15 16:52:36 +0200

    Code format

--- app/src/main/java/com/dimitrioskanellopoulos/athletica/AbstractTextPaint.java
+++ app/src/main/java/com/dimitrioskanellopoulos/athletica/AbstractTextPaint.java
@@ -6,6 +6,5 @@ import android.graphics.Paint;
  * A AbstractTextPaint is a normal paint that has just an extra placeholder for text
  */
 abstract class AbstractTextPaint extends Paint implements TextPaint{
-
     protected String text = "";
 }
