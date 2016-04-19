72a456b00b84e34cca0de8172bbabd62fed2e80d
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-20 01:52:57 +0200

    Trying to figure out the best approach to getting an average

--- wear/src/main/java/com/dimitrioskanellopoulos/athletica/sensors/AbstractCallbackSensor.java
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/sensors/AbstractCallbackSensor.java
@@ -3,9 +3,14 @@ package com.dimitrioskanellopoulos.athletica.sensors;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.util.Log;
+
+import java.lang.ref.WeakReference;
 
 public abstract class AbstractCallbackSensor implements CallbackSensorEventListener {
     private String TAG = "CallbackSensor";
@@ -18,6 +23,7 @@ public abstract class AbstractCallbackSensor implements CallbackSensorEventListe
 
     public interface OnSensorEventCallback {
         void handleOnSensorChangedEvent(SensorEvent value);
+        void handleOnSensorAverageChanged(Float average);
     }
 
     public AbstractCallbackSensor(@NonNull Context context, Integer sensorType, @NonNull OnSensorEventCallback changeCallback) {
@@ -47,6 +53,23 @@ public abstract class AbstractCallbackSensor implements CallbackSensorEventListe
     }
 
     @Override
+    public void getAverage(Integer time) {
+        SensorEventListener mListener = new SensorEventListener() {
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+
+        };
+        sensorManager.registerListener(mListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        isListening = true;
+        Log.d(TAG, "Monitoring average");
+    }
+
+    @Override
     public void onSensorChanged(SensorEvent event) {
         // Pass the value to the callback
         changeCallback.handleOnSensorChangedEvent(event);
@@ -56,4 +79,5 @@ public abstract class AbstractCallbackSensor implements CallbackSensorEventListe
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         Log.d(TAG, "Accuracy changed");
     }
+
 }
