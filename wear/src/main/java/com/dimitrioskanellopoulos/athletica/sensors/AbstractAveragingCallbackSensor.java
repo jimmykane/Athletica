9d31cf5abac37d176d32ff0d2e7716c5c4ec3254
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-20 21:51:46 +0200

    Implement averaging calling

--- wear/src/main/java/com/dimitrioskanellopoulos/athletica/sensors/AbstractAveragingCallbackSensor.java
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/sensors/AbstractAveragingCallbackSensor.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -16,15 +17,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractAveragingCallbackSensor extends AbstractCallbackSensor implements SensorAverageListenerInterface, OnSensorEventCallbackInterface, OnSensorAverageEventCallbackInterface {
+    private final static Integer samplingPeriodUs = 5000;
+    private final static Integer maxReportLatencyUs = 5000;
+
 
     private final AveragingSensorEventListener averagingSensorEventListener = new AveragingSensorEventListener(this);
 
-    public AbstractAveragingCallbackSensor(@NonNull Context context, Integer sensorType, @NonNull OnSensorEventCallbackInterface changeCallback, @NonNull OnSensorAverageEventCallbackInterface averageChangeCallback) {
+    protected final OnSensorAverageEventCallbackInterface averageChangeCallback;
+
+    public AbstractAveragingCallbackSensor(@NonNull Context context,
+           Integer sensorType,
+           @NonNull OnSensorEventCallbackInterface changeCallback,
+           @NonNull OnSensorAverageEventCallbackInterface averageChangeCallback
+    ) {
         super(context, sensorType, changeCallback);
+        this.averageChangeCallback = averageChangeCallback;
     }
 
     @Override
     public void getAverage() {
+        if (isListening()){
+            Log.d(TAG, "Average calculation aborted. Sensor is listening");
+            return;
+        }
+        Log.d(TAG, "Starting average calculation");
+        sensorManager.registerListener(averagingSensorEventListener, sensor, samplingPeriodUs, maxReportLatencyUs);
+    }
 
+    @Override
+    public void handleOnSensorAverageChangedEvent(SensorEvent event) {
+        Log.d(TAG, "Average calculated: " + String.format("%.01f", event.values[0]));
+        sensorManager.unregisterListener(averagingSensorEventListener);
+        Log.d(TAG, "Stopped listening");
+        if (isEventValueAcceptable(event)) {
+            averageChangeCallback.handleOnSensorAverageChangedEvent(event);
+        }
     }
 }
