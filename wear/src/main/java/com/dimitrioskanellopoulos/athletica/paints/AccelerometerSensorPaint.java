a1124c46465663b908ecbedd77c9dfa0a95a7904
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-04-29 23:55:59 +0200

    Change units

--- wear/src/main/java/com/dimitrioskanellopoulos/athletica/paints/AccelerometerSensorPaint.java
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/paints/AccelerometerSensorPaint.java
@@ -3,7 +3,7 @@ package com.dimitrioskanellopoulos.athletica.paints;
 public class AccelerometerSensorPaint extends SensorPaint {
 
     private final static String icon = "\uf00a";
-    private final static String units = "m/s2";
+    private final static String units = "m/s^2";
 
     @Override
     public String getIcon() {
