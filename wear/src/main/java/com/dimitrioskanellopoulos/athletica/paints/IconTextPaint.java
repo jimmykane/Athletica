b7fdb19729404b9874ec0b590531f2ef072c0dc2
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-05-03 15:56:02 +0200

    Add IconTextPaint

---
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/paints/IconTextPaint.java
@@ -0,0 +1,10 @@
+package com.dimitrioskanellopoulos.athletica.paints;
+
+import android.graphics.Typeface;
+
+public class IconTextPaint extends TextPaint {
+
+    public IconTextPaint(Typeface typeface){
+        setTypeface(typeface);
+    }
+}
