cd3edd78cdfa16acc3a1c886a0a937f6c134c8c0
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-05-05 11:49:19 +0200

    Do not initialise with a font

--- wear/src/main/java/com/dimitrioskanellopoulos/athletica/paints/IconTextPaint.java
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/paints/IconTextPaint.java
@@ -1,9 +1,5 @@
 package com.dimitrioskanellopoulos.athletica.paints;
 
-import android.graphics.Typeface;
-
 public class IconTextPaint extends TextPaint {
-    public IconTextPaint(Typeface typeface){
-        setTypeface(typeface);
-    }
+
 }
