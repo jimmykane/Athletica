5638814ba2ecdc693947c77f658866726fb1a5c0
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-05-06 20:56:18 +0200

    Add configuration activity

---
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/WatchFaceConfigurationActivity.java
@@ -0,0 +1,6 @@
+package com.dimitrioskanellopoulos.athletica;
+
+import android.support.wearable.activity.WearableActivity;
+
+public class WatchFaceConfigurationActivity extends WearableActivity {
+}
