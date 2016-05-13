391112cbba47d769dff15400af326801621ae4b2
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-05-13 21:47:12 +0200

    Change to heart rate

--- wear/src/main/java/com/dimitrioskanellopoulos/athletica/WatchFaceService.java
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/WatchFaceService.java
@@ -751,8 +751,8 @@ public class WatchFaceService extends CanvasWatchFaceService {
                 location.setAccuracy(3.0f);
                 updateSunriseAndSunset(location);
                 deactivateAllSensors();
-                watchFace.addSensorColumn(Sensor.TYPE_AMBIENT_TEMPERATURE);
-                watchFace.updateSensorPaintText(Sensor.TYPE_AMBIENT_TEMPERATURE, "21");
+                watchFace.addSensorColumn(Sensor.TYPE_HEART_RATE);
+                watchFace.updateSensorPaintText(Sensor.TYPE_HEART_RATE, "128");
             }
 
             calculateAverageForActiveSensors();
