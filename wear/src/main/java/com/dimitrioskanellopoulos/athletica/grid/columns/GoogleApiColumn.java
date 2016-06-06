3c53229327227642ff060ad57dd602d297d9a57e
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-06-06 20:55:59 +0200

    Add google api column

---
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/grid/columns/GoogleApiColumn.java
@@ -0,0 +1,34 @@
+package com.dimitrioskanellopoulos.athletica.grid.columns;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+public abstract class GoogleApiColumn extends Column implements GoogleApiColumnInterface {
+    private final static String TAG = "GoogleApiColumn";
+
+    public GoogleApiColumn(Context context, Typeface paintTypeface, Float paintTextSize, int paintColor) {
+        super(context, paintTypeface, paintTextSize, paintColor);
+    }
+
+    @Override
+    public void setIsVisible(Boolean isVisible) {
+        super.setIsVisible(isVisible);
+        if (isVisible) {
+            if (!getGoogleApiClient().isConnected()) {
+                getGoogleApiClient().connect();
+            }
+        } else {
+            if (getGoogleApiClient().isConnected()) {
+                getGoogleApiClient().disconnect();
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        if (getGoogleApiClient().isConnected()) {
+            getGoogleApiClient().disconnect();
+        }
+        super.destroy();
+    }
+}
---
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/grid/columns/GoogleApiColumnInterface.java
@@ -0,0 +1,7 @@
+package com.dimitrioskanellopoulos.athletica.grid.columns;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public interface GoogleApiColumnInterface {
+    GoogleApiClient getGoogleApiClient();
+}
