a37992f2a9097b9a60b5b957f33c6a733fecbd8a
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-06-11 22:23:58 +0200

    Change visibility

--- wear/src/main/java/com/dimitrioskanellopoulos/athletica/grid/GridRenderer.java
+++ wear/src/main/java/com/dimitrioskanellopoulos/athletica/grid/GridRenderer.java
@@ -122,7 +122,7 @@ public class GridRenderer {
         }
     }
 
-    public static void drawBackground(Canvas canvas, Rect bounds, Integer color) {
+    private static void drawBackground(Canvas canvas, Rect bounds, Integer color) {
         Paint backgroundPaint = new Paint();
         backgroundPaint.setColor(color);
         canvas.drawRect(0, 0, bounds.width(), bounds.height(), backgroundPaint);
